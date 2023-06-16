5136e14a732c5419041c616f2f17151be8606826
Huzail Isani <huzail.isani@gmail.com>
2023-06-16 17:26:55 +0500

    link

--- frontend/src/component/Register.js
+++ frontend/src/component/Register.js
@@ -301,7 +301,7 @@ const Register = () => {
         const { name, email, city, complain } = complaint;
 
         try {
-            const response = await fetch('http://localhost:5000/c/complaint', {
+            const response = await fetch('https://newcomplaint-s5tpj1kyu-huzailessani.vercel.app/c/complaint', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
